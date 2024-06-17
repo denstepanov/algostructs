2295e7e29b4cf2c6dff8f97e29b61b8d73274885
Stepanov Den <denis.stepanov.dev@gmail.com>
2024-06-17 22:16:33 +0500

    upd sortString

--- src/other/anagram/anagram.go
+++ src/other/anagram/anagram.go
@@ -7,14 +7,16 @@ func IsAnagram(str1, str2 string) bool {
 		return false
 	}
 
-	return sortString(str1) == sortString(str2)
+	sortString(&str1)
+	sortString(&str2)
+	return str1 == str2
 }
 
-func sortString(str string) string {
-	runes := []rune(str)
+func sortString(str *string) {
+	runes := []rune(*str)
 	sort.Slice(runes, func(i, j int) bool {
 		return runes[i] < runes[j]
 	})
 
-	return string(runes)
+	*str = string(runes)
 }
